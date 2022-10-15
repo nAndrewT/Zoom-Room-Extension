baccdadf8566e0adee14487f1d83056927ce1eac
Chris K <Chris-SourceCode@outlook.com>
2022-10-15 20:46:57 +0200

    Adjust nav

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
-const pastZoomLinks = JSON.parse(localStorage.getItem('zoomHistory') ?? "[]")
-const dateString = JSON.stringify( Date.now())
-pastZoomLinks.push({ dateString: window.location.href })
-if (pastZoomLinks.length > 5) pastZoomLinks.shift();
-localStorage.setItem("zoomHistory", JSON.stringify(pastZoomLinks))
+// const pastZoomLinks = JSON.parse(localStorage.getItem('zoomHistory') ?? "[]")
+// const dateString = JSON.stringify( Date.now())
+// pastZoomLinks.push({ dateString: window.location.href })
+// if (pastZoomLinks.length > 5) pastZoomLinks.shift();
+// localStorage.setItem("zoomHistory", JSON.stringify(pastZoomLinks))
 
 // alert(JSON.stringify(pastZoomLinks))
